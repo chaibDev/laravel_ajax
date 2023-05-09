b5ecbb0fe5d7419a9232a18c10ecd08775b8fdd0
chaibDev <cnechaib@hotmail.fr>
2023-05-09 09:53:35 +0200

    mj

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/home', function () {
+    return view('dashboard');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::get('/', [EmployeeController::class, 'index']);
 Route::post('/store', [EmployeeController::class, 'store'])->name('store');
@@ -9,3 +36,5 @@ Route::get('/fetchall', [EmployeeController::class, 'fetchAll'])->name('fetchAll
 Route::delete('/delete', [EmployeeController::class, 'delete'])->name('delete');
 Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
 Route::post('/update', [EmployeeController::class, 'update'])->name('update');
+
+require __DIR__ . '/auth.php';
